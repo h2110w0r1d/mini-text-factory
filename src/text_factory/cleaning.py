0a2dd01378a9f2acd9471e97d5e0aa54b6d90c12
Evelyn Wu <yw114@illinois.edu>
2026-07-18 17:12:08 -0500

    Document text cleaning function

--- src/text_factory/cleaning.py
+++ src/text_factory/cleaning.py
@@ -1,4 +1,5 @@
 def clean_text(text: str) -> str:
-	lower_text = text.lower()
-	words = lower_text.split()
-	return " ".join(words)
+    """Normalize text casing and whitespace."""
+    lower_text = text.lower()
+    words = lower_text.split()
+    return " ".join(words)
